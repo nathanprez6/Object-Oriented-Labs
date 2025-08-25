bea5519f1c83e17a4735d43c7eb64affdc865549
nathanprez6 <nathan.mq123@gmail.com>
2025-08-25 13:26:05 -0700

    error added

--- Lab01.py
+++ Lab01.py
@@ -1,6 +1,7 @@
 # Authors: Nathan Nguyen, Max Cirson
 # Date: Aug 25, 2025
-# Description: 
+# Description: Generates a random value between 1 and 100,
+# then prompts the user to guess it.
 
 import random
 
@@ -15,7 +16,8 @@ def main():
             Number of guesses
 
         Raises:
-        
+            ValueError: Caused by trying to convert a non-numeric string
+            to an integer using int()
     """
     
     print("- Guessing Game –")
@@ -23,20 +25,20 @@ def main():
     random_value = random.randint(1, 100)
 
     guess_value = int(input("I'm thinking of a number. Make a guess (1-100): "))
-
     guess_count = 1
 
     while guess_value != random_value:
-        if guess_value < random_value:
-            guess_value = int(input("Too low! Guess again: "))
-            guess_count += 1
-        elif guess_value > random_value:
-            guessValguess_valueue = int(input("Too high! Guess again: "))
-            guess_count += 1
-        elif guess_value < 1 or guess_value > 100:
-            print("Invalid input – should be within range 1-100.")
-            guess_value = int(input("Guess again (1-100): "))
-        else:
+        try:
+            if guess_value < random_value and guess_value >= 1:
+                guess_value = int(input("Too low! Guess again: "))
+                guess_count += 1
+            elif guess_value > random_value and guess_value <= 100:
+                guess_value = int(input("Too high! Guess again: "))
+                guess_count += 1
+            else:
+                print("Invalid input – should be within range 1-100.")
+                guess_value = int(input("Guess again (1-100): "))
+        except ValueError:
             print("Invalid input – should be an integer.")
             guess_value = int(input("Guess again (1-100): "))
 
