d8717bb7e800add571bf6c9d2fb5a3f47208f253
nathanprez6 <nathan.mq123@gmail.com>
2025-09-24 11:45:30 -0700

    doc

--- Lab05/contact.py
+++ Lab05/contact.py
@@ -2,7 +2,16 @@
 # Date: Sept. 22, 2025
 
 class Contact:
-    def _init__(self, fn, ln, ph, addr, city, zip):
+    def __init__(self, fn, ln, ph, addr, city, zip):
+        """ 
+        Constructor for the Contact class
+
+        Args:
+            self, fn, ln, ph, addr, city, zip: attributes of the Contact class
+
+        Returns: 
+            None 
+        """
         self.fn = fn
         self.ln = ln
         self.ph = ph
@@ -11,15 +20,42 @@ class Contact:
         self.zip = zip
 
     def __lt__(self, other):
+        """ 
+        Passes in two contacts and compares them by last names, if they are the same, then compare by first names.
+
+        Args:
+            self, other: Two Contacts to be compared and sorted
+
+        Returns: 
+            A boolean value that is true if the two contacts are the same and false otherwise
+        """
         if self.ln == other.ln:
             return self.fn < self.ln
         return self.ln < other.ln
 
     def __str__(self):
-        return (f"{self.f_name} {self.l_name}\n"
-                f"{self.phone}\n"
-                f"{self.address}\n"
+        """ 
+        A method to print the contact.
+
+        Args:
+            self: The Contact to be printed
+
+        Returns: 
+            A string that is used to display the contact to the console
+        """
+        return (f"{self.fn} {self.ln}\n"
+                f"{self.ph}\n"
+                f"{self.addr}\n"
                 f"{self.city}, {self.zip}")
 
     def __repr__(self):
+        """ 
+        A method to format the contact to be written to the file.
+
+        Args:
+            self: The Contact to be formatted and written
+
+        Returns: 
+            A string that is used to write the contact to the file
+        """ 
         return f"{self.fn},{self.ln},{self.ph},{self.addr},{self.city},{self.zip}"
--- Lab05/main.py
+++ Lab05/main.py
@@ -4,121 +4,161 @@
 # made up of contact objects.
 
 import check_input
-import contact
+from contact import Contact
 
 def read_file():
+    """ 
+    Open a file and reads in each contact (one per line), then construct a Contact
+    object using the data, and stores the Contact object in the list of contacts.
+
+    Args:
+        None
+
+    Returns: 
+        The list of contacts 
+    """
     contacts = []
-    with open("addresses.txt", 'r') as addresses_file:
-        for line in addresses_file:
-            parts = line.strip().split(",")
-            if len(parts) == 6:
-                fn, ln, ph, addr, city, zip_code = parts
-                contacts.append(contact.Contact(fn, ln, ph, addr, city, zip_code))
+    with open("addresses.txt", "r") as file:
+        for line in file:
+            fn, ln, ph, addr, city, zip = line.strip().split(",")
+            contacts.append(Contact(fn, ln, ph, addr, city, zip))
     contacts.sort()
     return contacts
 
 def write_file(contacts):
-    with open("addresses.txt", 'w') as addresses_file:
-        for contact in contacts:
-            addresses_file.write(repr(contact) + "\n")
+    """ 
+    Open a file for writing, loops through the contacts list and writes each contact
+    to the file using the repr method.
+
+    Args:
+        contacts (list): a list of contacts
+
+    Returns: 
+        None 
+    """
+    with open("addresses.txt", "w") as file:
+        for c in contacts:
+            file.write(repr(c) + "\n")
+    
 
 def get_menu_choice():
-    menu = """
-    Rolodex Menu:
-    1. Display Contacts
-    2. Add Contact
-    3. Search Contacts
-    4. Modify Contact
-    5. Save and Quit"""
-    print(menu)
+    """ 
+    Displays the main menu to the user and then prompts the user for a valid input.
 
-    user_choice = check_input.get_int_range(">", 1, 5)
-    return user_choice
+    Args:
+        None
+
+    Returns: 
+        An integer value of the user's choice 
+    """
+    print("Rolodex Menu:\n1. Display Contacts\n2. Add Contact\n" \
+    "3. Search Contacts\n4. Modify Contact\n5. Save and Quit")
+
+    menu_choice = check_input.get_int_range("Enter your choice (1-5): ", 1, 5)
+    return menu_choice
 
 def modify_contact(cont):
-    modify_menu = """
-    1. First name
-    2. Last name
-    3. Phone
-    4. Address
-    5. City
-    6. Zip
-    7. Save"""
+    """ 
+    Displays the modify menu to the user, gets the user’s valid input, then, based on the user’s choice, 
+    prompts the user for the information they’d like to change, and then updates the appropriate attribute
+    for the contact.
 
-    user_choice = 0
+    Args:
+        cont (Contact object): the Contact object to be modified
 
-    while user_choice != 7:
-        print(modify_menu)
-        user_choice = check_input.get_int_range(">", 1, 7)
+    Returns: 
+        None 
+    """
+    mod_menu = "\nModify Menu:\n1. First Name\n2. Last Name\n3. Phone \n4. Address\n5. City\n6. Zip \n7. Save"
+    repeat = True
 
-        if user_choice == 1:
+    while repeat:
+        print(mod_menu)
+        choice = check_input.get_int_range(">", 1, 7)
+        if choice == 1: 
             cont.fn = input("Enter first name: ")
-        elif user_choice == 2:
+        elif choice == 2: 
             cont.ln = input("Enter last name: ")
-        elif user_choice == 3:
-            cont.ph = input("Enter phone: ")
-        elif user_choice == 4:
+        elif choice == 3: 
+            cont.ph = input("Enter phone #: ")
+        elif choice == 4: 
             cont.addr = input("Enter address: ")
-        elif user_choice == 5:
+        elif choice == 5:
             cont.city = input("Enter city: ")
-        elif user_choice == 6:
+        elif choice == 6:
             cont.zip = input("Enter zip: ")
-        elif user_choice == 7:
-            break
+        elif choice == 7:
+            repeat = False
+        
 
 def main():
     contacts = read_file()
-    
-    menu_choice = 0
+    repeat = True
 
-    while menu_choice != 5:
-        menu_choice = get_menu_choice()
-
-        if menu_choice == 1:
-            # Display contacts
-            print("Number of contacts: " + len(contacts))
+    # Loops until user chooses to Save and Quit
+    while repeat:
+        choice = get_menu_choice()
+        if choice == 1:
+        # Displays Contact
+            print("Number of contacts: " + str(len(contacts)))
             for i, contact in enumerate(contacts, start = 1):
                 print(f"{i}. {contact}\n")
-        elif menu_choice == 2:
-            # Add contact
-            print("Enter new contact: ")
+        elif choice == 2:
+        # Add New Contact
+            print("Enter new contact:")
             fn = input("First name: ")
             ln = input("Last name: ")
             ph = input("Phone #: ")
             addr = input("Address: ")
             city = input("City: ")
             zip = input("Zip: ")
-            contacts.append(contact.Contact(fn, ln, ph, addr, city, zip))
+            print()
+
+            contacts.append(Contact(fn, ln, ph, addr, city, zip))
             contacts.sort()
-        elif menu_choice == 3:
-            # Search contacts
-            print("Search:\n1. Search by last name\n2. Search by zip")
-            search_by = check_input.get_int_range(">", 1, 2)
-            if search_by == 1:
-                ln_to_find = input("Enter last name: ")
-                for cont in contacts:
-                    if ln_to_find == cont.ln:
-                        str(cont)
-            else:
-                zip_to_find = input("Enter zip: ")
-                for cont in contacts:
-                    if zip_to_find == cont.zip:
-                        str(cont)
-        elif menu_choice == 4:
-            # Modify contact
-            fn_to_mod = input("Enter first name: ")
-            ln_to_mod = input("Enter last name: ")
-            for cont in contacts:
-                if fn_to_mod == cont.fn and ln_to_mod == cont.ln:
-                    str(cont)
-                    modify_contact(cont)
-            contacts.sort()
-        elif menu_choice == 5:
-            # Save and quit
-            print("Saving File...")
             write_file(contacts)
+        elif choice == 3:
+        # Search Contact
+            search_choice = check_input.get_int_range("Search:\n1. Search by last name\n2. Search by Zip\n>", 1, 2)
+            if search_choice == 1:
+            # Search by Last Name
+                ln_choice = input("Enter last name: ")
+                found = False
+                for c in contacts:
+                    if c.ln == ln_choice:
+                        found = True
+                        print(c)
+                        print()
+                if not found:
+                    print("Contact not found.")
+            if search_choice == 2:
+            # Search by Zip
+                zip_choice = input("Enter Zip: ")
+                found = False
+                for c in contacts:
+                    if c.zip == zip_choice:
+                        found = True
+                        print(c)
+                        print()
+                if not found:
+                    print("Contact not found.")
+        elif choice == 4:
+        # Modify Contact
+            fn_choice = input("Enter first name: ")
+            ln_choice = input("Enter last name: ")
+            found = False
+            for c in contacts:
+                if c.fn == fn_choice and c.ln == ln_choice:
+                    found = True
+                    str(c)
+                    modify_contact(c)
+            contacts.sort()
+            if not found:
+                print("Contact not found.")
+        elif choice == 5:
+        # Save and Quit
+            repeat = False
+            write_file(contacts)
+            print("Saving File...\nEnding Program")
 
-    print("Ending Program")
-
-
-main()
+main()  
